333394c78e467501b887312406bef648cda6efbb
suudi4 <suudi08@gmail.com>
2023-09-24 07:58:40 +0000

    first commit from end

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <h1>This is a just a demo site for exploring how IVR response can be shared back with client using API</h1>
       
     </main>
   )
