a8fff4e129f1c1d9dd6d1b82c756be7034f3e61f
suudi4 <suudi08@gmail.com>
2023-09-25 05:57:07 +0000

    process

---
+++ app/api/ivr/whatsapp/sendmessage/route.ts
@@ -0,0 +1,37 @@
+// pages/api/whatsapp/sendMessage.ts
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+type error:any
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    try {
+      const response = await axios.post(
+        'https://graph.facebook.com/v17.0/102992352518213/messages',
+        {
+          messaging_product: 'whatsapp',
+          to: '252634008081',
+          type: 'template',
+          template: {
+            name: 'hello_world',
+            language: {
+              code: 'en_US',
+            },
+          },
+        },
+        {
+          headers: {
+            Authorization: process.env.fb_accesstoken,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      res.status(200).json(response.data);
+    } catch (error) {
+      res.status( 500).json( {});
+    }
+  } else {
+    res.status(405).end(); // Method Not Allowed
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      <h1>This is a just a demo site for exploring how IVR response can be shared back with client using API</h1>
+      <h1>This is a just a demo site for exploring how IVR response can be shared back with client using API
+
+        {process.env.fb_accesstoken}
+      </h1>
       
     </main>
   )
