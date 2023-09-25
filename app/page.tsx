5356f120f4a0d6ab10f3a6eddb19fb8f3b5628dc
suudi4 <suudi08@gmail.com>
2023-09-25 06:07:25 +0000

    process

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 
 export default function Home() {
+  console.log(process.env.fb_accesstoken)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
       <h1>This is a just a demo site for exploring how IVR response can be shared back with client using API
 
-        {process.env.fb_accesstoken}
+        
       </h1>
       
     </main>
