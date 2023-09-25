fb06662a446056518d8e3e2f3169abccc3dc0441
suudi4 <suudi08@gmail.com>
2023-09-25 06:01:48 +0000

    process

--- app/api/ivr/whatsapp/sendmessage/route.ts
+++ app/api/ivr/whatsapp/sendmessage/route.ts
@@ -2,7 +2,7 @@
 
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
-type error:any
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     try {
